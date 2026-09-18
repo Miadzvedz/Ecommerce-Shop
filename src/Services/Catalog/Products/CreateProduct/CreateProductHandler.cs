689c6f9c3ad1dcb77c2701e2c91682963e60f2dd
Alexander Medved <medved1991alexander@gmail.com>
2026-09-18 23:50:14 +0300

    change version ubuntu-22.04

--- src/Services/Catalog/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Products/CreateProduct/CreateProductHandler.cs
@@ -12,7 +12,7 @@ public class CreateProductCommandValidator : AbstractValidator<CreateProductComm
     {
         RuleFor(i => i.Name)
             .NotEmpty().WithMessage("Is required")
-            .Length(2, 100).WithMessage("must be between 2 and 100 characters");
+            .Length(1, 100).WithMessage("must be between 1 and 100 characters");
         RuleFor(i => i.Category).NotEmpty().WithMessage("Is required");
         RuleFor(i => i.ImageFile).NotEmpty().WithMessage("Is required");
         RuleFor(i => i.Price).GreaterThan(0).WithMessage("Must be greater than 0");
